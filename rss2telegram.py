5b242a290cdef7be6f85bc6f847ee656d94e3ff1
Gustavo Saez <github@podapps.net>
2022-07-09 22:22:04 -0300

    Update rss2telegram.py

--- rss2telegram.py
+++ rss2telegram.py
@@ -53,7 +53,7 @@ def check_topics(url):
     feed = feedparser.parse(url)
     for topic in reversed(feed['items'][:10]):
         source = feed['feed']['title']
-        title = f'🗞 <b>{topic.title}</b>\n\n🔗 <a href=“{topic.link}”>{source}</a>'
+        title = f'🗞 <b>{topic.title}</b>\n\n🔗 <a href="{topic.link}">{source}</a>'
         link = topic.links[0].href
         photo = get_img(topic.links[0].href)
         if not check_history(link):
